7c51753b111134383e51d9d667671c1650ed75ae
Dev-Souza <kauan.souza.dev@gmail.com>
2025-06-03 21:40:05 -0300

    feat: create model the categories

---
+++ src/main/java/com/project/ToDoList/models/categories/CorCategoryEnum.java
@@ -0,0 +1,13 @@
+package com.project.ToDoList.models.categories;
+
+public enum CorCategoryEnum {
+    VERMELHO,
+    AZUL,
+    VERDE,
+    AMARELO,
+    LARANJA,
+    ROXO,
+    ROSA,
+    CINZA,
+    PRETO
+}
---
+++ src/main/java/com/project/ToDoList/models/categories/TipoCategoryEnum.java
@@ -0,0 +1,14 @@
+package com.project.ToDoList.models.categories;
+
+public enum TipoCategoryEnum {
+    PESSOAL,
+    PROFISSIONAL,
+    ESTUDOS,
+    SAUDE,
+    FINANCAS,
+    LAZER,
+    CASA,
+    VIAGEM,
+    COMPRAS,
+    OUTROS
+}
