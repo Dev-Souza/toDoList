8c60bb749826dd5773188ef9284b4d2f62e31dda
Dev-Souza <kauan.souza.dev@gmail.com>
2025-06-03 21:41:10 -0300

    fix: correct the category

--- src/main/java/com/project/ToDoList/models/categories/CategoriesModel.java
+++ src/main/java/com/project/ToDoList/models/categories/CategoriesModel.java
@@ -1,4 +1,34 @@
 package com.project.ToDoList.models.categories;
 
+import com.project.ToDoList.models.tasks.TasksModel;
+import com.project.ToDoList.models.users.UsersModel;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Table(name = "categories")
+@Setter
+@Getter
+@Entity
 public class CategoriesModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nameCategory;
+    @Column(nullable = false)
+    private String descriptionCategory;
+    @Enumerated(EnumType.STRING)
+    private CorCategoryEnum corCategoryEnum;
+    @Enumerated(EnumType.STRING)
+    private TipoCategoryEnum tipoCategoryEnum;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<TasksModel> tasks = new ArrayList<>();
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UsersModel user_id;
 }
